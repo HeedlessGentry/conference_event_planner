9d1bb2aa77b97d210b30c0baaf5ddcb744aebed4
HeedlessGentry <chrisgithinji286@gmail.com>
2024-11-19 04:40:22 -0500

    Debugging

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -85,7 +85,34 @@ const ConferenceEvent = () => {
         console.log(items);
         return <>
         <div className="display_box1">
-            {items.lenght}
+            {items.length === 0 && <p>No items selected</p>}
+            <table className="table_item_data">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Unit Cost</th>
+                        <th>Quantity</th>
+                        <th>Subtotal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {items.map((item, index) => (
+                        <tr key = {index}>
+                            <td>{item.name}</td>
+                            <td>${item.cost}</td>
+                            <td>
+                                {item.type === "meals" || item.numberOfPeople
+                                ? `For ${numberOfPeople} people`
+                            : item.quantity}
+                            </td>
+                            <td> {item.type === "meals" || item.numberOfPeople
+                            ? `${item.cost * numberOfPeople}`
+                        : `${item.cost * item.quantity}`}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
         </>
     };
